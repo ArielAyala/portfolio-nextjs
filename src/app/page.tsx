92de78ca00fb8e77543af621cd84d09789a61b55
Ariel Ayala <arielayalamail@gmail.com>
2024-02-01 19:22:46 -0300

    Organize sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
-        {/* <HeroSection />
-        <AchievementsSection />
+        <HeroSection />
+        {/* <AchievementsSection /> */}
         <AboutSection />
         <ProjectsSection />
-        <EmailSection /> */}
+        <ContactSection />
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 }
