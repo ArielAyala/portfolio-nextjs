a93fcebf66e8d774d4b0de76003653ddbe7eea30
Ariel Ayala <arielayalamail@gmail.com>
2024-02-02 09:22:25 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/github-pages',
+    basePath: "/portfolio-nextjs",
 };
 
 export default nextConfig;
