eea73f98631e656ca65e4f6dea1f432b9d414d46
Ariel Ayala <arielayalamail@gmail.com>
2024-02-03 19:44:57 -0300

    Test image src

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
-import aboutImage from '../../../public/images/about-image.png';
+import aboutImage from 'images/about-image.png';
+//import aboutImage from '../../../public/images/about-image.png';
 import TabButton from "./TabButton";
 
 
