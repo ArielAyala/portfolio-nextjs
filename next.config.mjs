846e8a707b6efd36dac093a2d594a5c449360b55
Ariel Ayala <arielayalamail@gmail.com>
2024-02-06 09:48:39 -0300

    Test config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/portfolio-nextjs",
-  assetPrefix: "/portfolio-nextjs/",
-  images: {
-    loader: "default",
-  },
+  // basePath: "/portfolio-nextjs",
+  // assetPrefix: "/portfolio-nextjs/",
 };
 
 export default nextConfig;
