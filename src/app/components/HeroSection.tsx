bd9cd88d84e0988a04ce2b3be7c3d88c86151bcc
Ariel Ayala <arielayalamail@gmail.com>
2024-02-06 17:22:25 -0300

    Fix images path

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -57,7 +57,7 @@ const HeroSection = () => {
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               unoptimized
-              src="/portfolio-nextjs/images/profile-avatar.png"
+              src="/images/profile-avatar.png"
               alt="profile image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
