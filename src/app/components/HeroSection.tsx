80c806fe272e9648d52609fbc3da1a5b9f47b6b7
Ariel Ayala <arielayalamail@gmail.com>
2024-02-06 10:38:38 -0300

    Fix image src

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -60,7 +60,7 @@ const HeroSection = () => {
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
+            <img
               src="/portfolio-nextjs/images/profile-avatar.png"
               alt="profile image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
