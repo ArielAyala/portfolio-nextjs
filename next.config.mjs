1d8644ee653e7420a25357a29febabda98ef6880
Ariel Ayala <arielayalamail@gmail.com>
2024-02-06 09:37:33 -0300

    Tes config images src

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   output: "export",
   basePath: "/portfolio-nextjs",
   assetPrefix: "/portfolio-nextjs/",
+  images: {
+    disableStaticImages: true,
+  },
 };
 
 export default nextConfig;
