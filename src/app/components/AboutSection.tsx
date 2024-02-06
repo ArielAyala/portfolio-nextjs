fa4be35a61ff52bb5446807a93dfa484cb8b8087
Ariel Ayala <arielayalamail@gmail.com>
2024-02-06 09:59:24 -0300

    Test img src fix

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -2,6 +2,7 @@
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
+import aboutImage from '../public/images/about-image.jpg'
 
 
 
@@ -53,7 +54,8 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.jpg" alt="About image" />
+        <Image src={aboutImage} alt="About image" />
+        {/* <Image src="/images/about-image.jpg" alt="About image" /> */}
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg text-justify">
