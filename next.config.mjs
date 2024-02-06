e5f223731fc0956f3c65a47e6c3d902b88dd8749
Ariel Ayala <arielayalamail@gmail.com>
2024-02-06 08:39:15 -0300

    FIx config to test image src

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   basePath: "/portfolio-nextjs",
-  assetPrefix: '/portfolio-nextjs/',
+  assetPrefix: "/portfolio-nextjs/",
   images: {
-    disableStaticImages: true,
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+    loader: "imgix",
   },
 };
 
