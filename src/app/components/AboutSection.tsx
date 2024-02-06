b769982c44ed16a6c24453eb3d225cd648af423f
Ariel Ayala <arielayalamail@gmail.com>
2024-02-06 10:25:49 -0300

    Test image src

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -2,9 +2,6 @@
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
-import aboutImage from '../../../public/images/about-image.jpg'
-
-
 
 const TAB_DATA = [
   {
@@ -34,8 +31,22 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li><a href="https://platzi.com/p/Ariel_Ayala/ruta/8-desarrollo-react/diploma/detalle/" target="_black">Frontend in React - Platzi</a></li>
-        <li><a href="https://platzi.com/p/Ariel_Ayala/ruta/44-desarrollo-java/diploma/detalle/" target="_black">Development in Java - Platzi</a></li>
+        <li>
+          <a
+            href="https://platzi.com/p/Ariel_Ayala/ruta/8-desarrollo-react/diploma/detalle/"
+            target="_black"
+          >
+            Frontend in React - Platzi
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://platzi.com/p/Ariel_Ayala/ruta/44-desarrollo-java/diploma/detalle/"
+            target="_black"
+          >
+            Development in Java - Platzi
+          </a>
+        </li>
       </ul>
     ),
   },
@@ -54,14 +65,14 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src={aboutImage} alt="About image" />
-        {/* <Image src="/images/about-image.jpg" alt="About image" /> */}
+        <Image src="/about-image.jpg" alt="About image" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg text-justify">
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with NetCore/C#, Angular, SQL Server, MySQL, Ionic, Javascript, Typescript and Git. I am a quick learner and I am always
+            working with NetCore/C#, Angular, SQL Server, MySQL, Ionic,
+            Javascript, Typescript and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create challenging applications.
           </p>
